9549d6f9a4545d242f154324a8cb8d806ee5fafa
Alvin Prayuda <alvinprayuda@gmail.com>
2025-01-30 09:20:11 +0700

    feat: refactor generate-descriptions using pydantic

--- settings.py
+++ settings.py
@@ -1,10 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict, YamlConfigSettingsSource, PydanticBaseSettingsSource
+from typing import Type, Tuple
+
 class Settings(BaseSettings):
     project_id: str
     location: str
     gemini_model_name: str
 
     model_config = SettingsConfigDict(
-        yaml_file="settings.yaml", yaml_file_encoding="utf-8"
+        yaml_file="config.yaml", yaml_file_encoding="utf-8"
     )
 
     @classmethod
@@ -17,3 +20,6 @@ class Settings(BaseSettings):
         file_secret_settings: PydanticBaseSettingsSource,
     ) -> Tuple[PydanticBaseSettingsSource, ...]:
         return (YamlConfigSettingsSource(settings_cls),)
+    
+def get_settings() -> Settings:
+    return Settings()
