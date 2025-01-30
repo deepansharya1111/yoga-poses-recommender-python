8ceb15684941f88811f056d9016ae14e17ce6c2f
Alvin Prayuda <alvinprayuda@gmail.com>
2025-01-30 09:55:14 +0700

    feat: update search-data with pydantic settings

--- search-data.py
+++ search-data.py
@@ -1,19 +1,21 @@
 """Module providing an example of Firestore Vector Similarity Search"""
+
 import argparse
 import logging
 
-import os
-from dotenv import load_dotenv
 from google.cloud import firestore
 from langchain_core.documents import Document
 from langchain_google_firestore import FirestoreVectorStore
 from langchain_google_vertexai import VertexAIEmbeddings
+from settings import get_settings
 
-load_dotenv()
+settings = get_settings()
+
 
 logging.basicConfig(
-        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-    )
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
 
 def parse_arguments():
     """Parses command-line arguments."""
@@ -27,31 +29,28 @@ def parse_arguments():
     )
     return parser.parse_args()
 
-def search(query:str):
+
+def search(query: str):
     """Executes Firestore Vector Similarity Search"""
     embedding = VertexAIEmbeddings(
-            model_name=os.getenv("EMBEDDING_MODEL_NAME"),
-            project=os.getenv("PROJECT_ID"),
-            location=os.getenv("LOCATION")
+        model_name=settings.embedding_model_name,
+        project=settings.project_id,
+        location=settings.location,
     )
 
-    client = firestore.Client(
-           project=os.getenv("PROJECT_ID"),
-           database=os.getenv("DATABASE")
-           )
-    
+    client = firestore.Client(project=settings.project_id, database=settings.database)
+
     vector_store = FirestoreVectorStore(
-                        client=client,
-                        collection=os.getenv("COLLECTION"),
-                        embedding_service=embedding)
-   
+        client=client, collection=settings.collection, embedding_service=embedding
+    )
+
     logging.info(f"Now executing query: {query}")
     results: list[Document] = vector_store.similarity_search(
-                                    query=query,
-                                    k=int(os.getenv("TOP_K")),
-                                    include_metadata=True)
+        query=query, k=int(settings.top_k), include_metadata=True
+    )
     for result in results:
         print(result.page_content)
+
 
 # Example: Suggest some exercises to relieve back pain
 # python search-data.py --prompt "Suggest some exercises to relieve back pain"
@@ -59,4 +58,3 @@ if __name__ == "__main__":
     args = parse_arguments()
     prompt = args.prompt
     search(prompt)
-
